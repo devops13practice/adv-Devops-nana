a8edfd19001c876ad673a1f1143e909c85b3acc4
dev-A <aoracletest@gmail.com>
2024-02-26 00:10:05 +0530

    new version of main.tf, renamed existing .tfvars to terraform.tfvars

--- main.tf
+++ main.tf
@@ -1,31 +1,20 @@
-variable "cidr_blocks" {
-  description = "vpc & subnet cidr blocks"
-  type = list(object({
-    cidr_block = string
-    name = string
-  }))
-}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
 
-resource "aws_vpc" "development-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
     tags = {
-      Name: var.cidr_blocks[0].name
+      Name: "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = "ap-south-1a"
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone =var.avail_zone
     tags = {
-      Name: var.cidr_blocks[1].name
+      Name: "${var.env_prefix}-subnet-1"
     }
-}
-
-output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
-}
-
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
 }
